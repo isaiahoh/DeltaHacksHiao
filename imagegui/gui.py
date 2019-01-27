1d85e04313537ddcd87e296cfe7041c60f04dba3
OussamaSaoudi <oussama.sa216@gmail.com>
2019-01-27 00:35:35 -0500

    Added sidebar and insert file for filepath search function

--- imagegui/gui.py
+++ imagegui/gui.py
@@ -12,12 +12,13 @@ from kivy.app import App
 from kivy.lang import Builder
 from kivy.uix.widget import Widget
 from kivy.config import Config
-from kivy.uix.screenmanager import ScreenManager, Screen
+from kivy.uix.screenmanager import ScreenManager, Screen, NoTransition
 from kivy.uix.boxlayout import BoxLayout
 from kivy.uix.image import Image
 
 
-
+class InitiationScreen(Screen):
+    pass
 class OriginalScreen(Screen):
     pass
 class DiscernedScreen(Screen):
@@ -32,12 +33,15 @@ presentation = Builder.load_file("imagedisplay.kv")
 class windowElements(BoxLayout):
     pass
 
-
-
 #sets the window and the elemnts that will be run
 class mainWindow(App):
     def build(self):
+        Window.bind(on_dropfile=self._on_file_drop)
         return presentation
+
+    def _on_file_drop(self, window, file_path):
+        self.root.current = "original"
+        return file_path
 # initiates the running of the program
 main = mainWindow()
 main.run()
