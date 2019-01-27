748add7acbb44df049a54460dae124255e2badc7
isaiahoh <turtlengou@gmail.com>
2019-01-27 00:15:45 -0500

    Creating image with contours

--- ImageRecognition/venv/Scripts/imageDeconstruction.py
+++ ImageRecognition/venv/Scripts/imageDeconstruction.py
@@ -2,18 +2,30 @@ import cv2
 import numpy as np
 import matplotlib as plt
 
-img = cv2.imread('C:\\Users\\ashar\\Documents\\git\\DeltaHacksHiao\\ImageRecognition\\image_4.png', cv2.IMREAD_COLOR)
+img = cv2.imread('C:\\Users\\turtl\\git\\DeltaHacksHiao\\ImageRecognition\\image_4.png', cv2.IMREAD_COLOR)
 #Input image location
-def imageProcess(img):
-    blur = cv2.GaussianBlur(img, (15, 15), 0)
+def imagePercentProcess(img):
+    blur = cv2.medianBlur(img, 17)
     retval, threshold = cv2.threshold(blur, 117, 255, cv2.THRESH_BINARY)
+    grey = cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
+    adapt = cv2.adaptiveThreshold(grey, 255, cv2.ADAPTIVE_THRESH_MEAN_C, cv2.THRESH_BINARY, 255, 15)
     print(threshold)
-    cv2.imshow('image', img)
+    cv2.imshow('image', blur)
+    cv2.imshow("adapt", adapt)
     cv2.imshow("threshold", threshold)
     cv2.waitKey(0)
     cv2.destroyAllWindows()
 
     return threshold
+def imageNumberProcess(img):
+    retval2, threshold2 = cv2.threshold(img, 172, 255, cv2.THRESH_BINARY)
+    contours, hierarchy = cv2.findContours(threshold2, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_NONE)
+    cv2.drawContours(img,contours, -1, 1, None, cv2.LINE_8, hierarchy, 1, None)
+
+    cnt = contours[0]
+    area = cv2.contourArea(cnt)
+    cv2.imshow("contours", img)
+    return area
 
 def colorPercent(threshold):
     img = threshold
@@ -33,4 +45,11 @@ def colorPercent(threshold):
         return (print('white pixel percentage:', np.round(ratio_color*100, 2)))
 
 
-colorPercent(imageProcess(img))
+colorPercent(imagePercentProcess(img))
+
+
+willitwork = imageNumberProcess(img)
+
+print(willitwork)
+
+cv2.contour
